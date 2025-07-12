93002564467f2ca940693f811b8b7ae1f659c3de
Savaliya Aayush Rameshbhai <aayushsavaliya235@gmail.com>
2025-07-12 23:09:03 +0530

    Homepage.jsx for blog is cpmlt

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar'
 import Header from '../components/Header'
 import Bloglist from '../components/Bloglist'
 import Newsletter from '../components/Newsletter'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Header/>
       <Bloglist/>
       <Newsletter/>
+      <Footer/>
     </>
   )
 }
