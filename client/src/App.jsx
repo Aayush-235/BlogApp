01720b435d5f2ac8c0dd7ad7ea85895113496b91
Savaliya Aayush Rameshbhai <aayushsavaliya235@gmail.com>
2025-07-20 12:23:03 +0530

    implement appContext for global state management; add axios and react-hot-toast dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,21 +9,25 @@ import Dashboard from './pages/admin/Dashboard'
 import Comments from './pages/admin/Comments'
 import Login from './components/admin/Login'
 import 'quill/dist/quill.snow.css'
+import { Toaster } from 'react-hot-toast'
+import { useAppContext } from './context/appContext.jsx'
 
 const App = () => {
+  const {token} = useAppContext()
   return (
     <div>
+      <Toaster />
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/blog/:id" element={<Blog/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/blog/:id" element={<Blog />} />
 
         {/* Admin Routes */}
         {/* nested routing setup */}
-        <Route path='/admin' element={true ? <Layout/> : <Login/>}>
-          <Route index element={<Dashboard/>}/>
-          <Route path="addblog" element={<Addblog/>} />
-          <Route path="listblog" element={<Listblog/> }/>
-          <Route path="comments" element={<Comments/>}/>
+        <Route path='/admin' element={token ? <Layout /> : <Login />}>
+          <Route index element={<Dashboard />} />
+          <Route path="addblog" element={<Addblog />} />
+          <Route path="listblog" element={<Listblog />} />
+          <Route path="comments" element={<Comments />} />
         </Route>
 
       </Routes>
---
+++ client/src/context/appContext.jsx
@@ -0,0 +1,63 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+
+const backendUrl = import.meta.env.VITE_BACKEND_URL
+axios.defaults.baseURL = backendUrl
+
+const appContext = createContext()
+
+export const AppProvider = ({ children }) => {
+
+    const navigate = useNavigate()
+
+    const [token, setToken] = useState(null);
+    const [blog, setBlog] = useState([]);
+    const [input, setInput] = useState('');
+
+    useEffect(() => {
+        fetchBlogs();
+        const token = localStorage.getItem('token');
+
+        if (token) {
+            setToken(token)
+            axios.defaults.headers.common['Authorization'] = `${token}`
+        }
+    }, [])
+
+    // Function fro fetch blogs
+    const fetchBlogs = async () => {
+        try {
+            const { data } = await axios.get('/api/blog/all')
+            if (data.success) {
+                setBlog(data.blogs)
+            }
+            else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            toast.error(error.message);
+
+        }
+    }
+
+
+
+
+
+
+    const value = {
+        axios, token, setToken, blog, setBlog, input, setInput, navigate
+    }
+
+    return (
+        <appContext.Provider value={value}>
+            {children}
+        </appContext.Provider>
+    )
+}
+
+export const useAppContext = () => {
+    return useContext(appContext)
+}
