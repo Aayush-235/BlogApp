8ed86a17db986b8de3146a4e079af557191a4327
Savaliya Aayush Rameshbhai <aayushsavaliya235@gmail.com>
2025-07-26 13:06:00 +0530

    Generate the blog description with Gemini ai is work properly

--- client/src/pages/admin/Addblog.jsx
+++ client/src/pages/admin/Addblog.jsx
@@ -3,12 +3,13 @@ import { assets, blogCategories } from '../../assets/assets'
 import Quill from 'quill'
 import { useAppContext } from '../../context/appContext.jsx'
 import toast from 'react-hot-toast'
+import { parse } from 'marked'
 
 const Addblog = () => {
 
   const { axios } = useAppContext()
   const [isAdding, setIsAdding] = useState(false);
-
+  const [loading, setLoading] = useState(false);
 
   const editorRef = useRef(null)
   const quillRef = useRef(null)
@@ -20,7 +21,26 @@ const Addblog = () => {
   const [isPublished, setIsPublished] = useState(false);
 
   const generateBlogWithAI = async () => {
+    if (!title) return toast.error('Please enter a title first');
 
+    try {
+      setLoading(true);
+      const { data } = await axios.post('api/blog/generateContent', { prompt: title })
+      if (data.success) {
+        quillRef.current.root.innerHTML = parse(data.content); // convert markdown text (generate by ai) to HTML format
+
+        //for an example
+
+        parse("# Hello") 
+         Output: <h1>Hello</h1>
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
   }
 
   const onSubmitHandler = async (e) => {
@@ -37,12 +57,10 @@ const Addblog = () => {
         description: quillRef.current.root.innerHTML,
       }
 
-        
+
       const formData = new FormData();
       formData.append('image', image);
       formData.append('blog', JSON.stringify(blog))
-
-
 
       const { data } = await axios.post('/api/blog/add', formData)
 
@@ -96,11 +114,14 @@ const Addblog = () => {
         <p className='mt-4'>Blog description</p>
         <div className='max-w-lg h-74 pb-16 sm:pb-10 pt-2 relative'>
 
-          <div ref={editorRef}>
-
-          </div>
-
-          <button className='absolute bottom-1 right-2 ml-2 text-xs text-white bg-black/70 px-4 py-1.5 rounded hover:underline cursor-pointer' type='button' onClick={generateBlogWithAI}>Generate with AI</button>
+          <div ref={editorRef}></div>
+          {loading && (
+            <div className='absolute top-0 right-0 bottom-0 left-0 flex items-center justify-center bg-black/10 mt-2 '>
+              <div className='w-8 h-8 rounded-full border-2 border-t-white animate-spin'>
+              </div>
+            </div>
+          )}
+          <button disabled={loading} className='absolute bottom-1 right-2 ml-2 text-xs text-white bg-black/70 px-4 py-1.5 rounded hover:underline cursor-pointer' type='button' onClick={generateBlogWithAI}>Generate with AI</button>
 
         </div>
 
